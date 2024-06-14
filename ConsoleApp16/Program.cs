44a97a122c7b6c1aec55968ac1063f2fe171f57a
CauanLAguiar <132419025+CauanLAguiar214210@users.noreply.github.com>
2024-06-14 14:26:37 -0300

    Commit1
    Inicio

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ConsoleApp16.Entities;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment(Console.ReadLine());
+            Comment c2 = new Comment("Wow that's awesome!");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'am going to visit this wonderful country",
+                12
+                );
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Console.WriteLine(p1);
+        }
+    }
+}
